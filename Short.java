e468b7a00772d9b58e76fd7566916a2fe8e5ba61
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-20 21:44:57 +0530

    short programme

---
+++ Short.java
@@ -0,0 +1,15 @@
+class Short{
+     static short []steps = {423,56,374,36,807,463,354,243,234,768};
+     public static void main(String[] args){
+     System.out.println(steps[0]);
+     System.out.println(steps[1]);
+     System.out.println(steps[2]); 
+     System.out.println(steps[3]);
+     System.out.println(steps[4]);
+     System.out.println(steps[5]);
+     System.out.println(steps[6]);
+     System.out.println(steps[7]);
+     System.out.println(steps[8]);
+     System.out.println(steps[9]);
+     }
+}
